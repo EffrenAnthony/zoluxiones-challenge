c89433dc3f485f441a55490fd8e353904cfb61fe
tony <effrenanthony2704@gmail.com>
2021-05-16 22:07:02 -0500

    modifying webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
+const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 module.exports = {
   output: {
+    path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js',
     publicPath: '/'
   },
